8408d48a5b325d14bbbfa0737695a2a347ae1ea7
The Lienhard <60452107+SupremeEvilOverlord@users.noreply.github.com>
2021-05-21 10:44:35 -0700

    Update game-final.js

--- game-final/game-final.js
+++ game-final/game-final.js
@@ -86,12 +86,12 @@ function title() {
   text('Squeaky’s Revenge:', w/2, h/5);
   textSize(30);
   text('Click to start', w/2, h/2);
-}
-  textSize(20);
+   textSize(20);
   text('Game Functions:', w/3, h/3); 
   text('left & right arrows to move', w/2, h/4);
   text('SpaceBar to shoot (in level 2)', w/2, h/5); 
 
+}
 function titleMousedClicked() {
     console.log('canvas is clicked on title page');
     state = 'level 1'
